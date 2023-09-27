3f143bfc6ebc5c8b346f6b41a1fbaa5646882a73
Zilfs <rheinlandcast@gmail.com>
2023-09-27 13:26:22 +0700

    update view dashboard kelas

--- resources/views/pages/management/dashboard-kelas.blade.php
+++ resources/views/pages/management/dashboard-kelas.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.dashboard')
 @section('page-content')
-    <div class="flex card mx-4 px-4 py-4">
+    <div class="flex card mx-4 px-4 py-4 my-5">
+        <h1 style="color: black">Data Kelas</h1>
         <table class="table table-hover">
-            <thead>
+            <thead style="color: black">
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">Nama Kelas</th>
                     <th scope="col">Jurusan</th>
+                    <th scope="col">Aksi</th>
                 </tr>
             </thead>
             <tbody>
@@ -18,9 +20,14 @@
                         <td>{{ $no++ }}</td>
                         <td>{{ $item->nama_kelas }}</td>
                         <td>{{ $item->jurusan }}</td>
+                        <td class="w-25"><a href="" class="btn btn-sm btn-warning mx-2 w-25">Edit</a><a
+                                href="" class="btn btn-sm btn-danger mx-2 w-25">Hapus</a></td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
+    </div>
+    <div class="flex card mx-4 px-4 py-4">
+        <a href="" class="btn btn-primary w-full">+ Tambah Data</a>
     </div>
 @endsection
