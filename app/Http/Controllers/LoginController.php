87c14771326f6cdba8ed05424aeb3bd7ea003766
anony11301 <fredriksowald54@gmail.com>
2023-09-27 10:02:21 +0700

    Commit 2

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     //
-    public function index() {
+    public function index() 
+    {
+        if ($users = Auth::user()) {
+            if ($users->level == '') {
+                return redirect()->intended('guru');
+            } elseif ($users->level == 'tu') {
+                return redirect()->intended('tu');
+            }
+        }
+
         return view('login.index');
     }
 }
