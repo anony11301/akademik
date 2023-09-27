9082bffab93c159f9790e9dde92b59aa57de464e
DikaPutra1205 <duia.putra12@gmail.com>
2023-09-27 11:45:38 +0700

    new migration

---
+++ database/migrations/2023_09_27_043910_create_absen_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('absen', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('NIS')->references('NIS')->on('siswa');
+            $table->foreign('id_kelas')->references('id')->on('kelas');
+            $table->string('NIS');
+            $table->date('tanggal');
+            $table->string('status');
+            $table->text('keterangan')->nullable();
+            $table->unsignedBigInteger('id_kelas');
+
+            
+            $table->timestamps();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('absen');
+    }
+};
