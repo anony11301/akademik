5d28488947ab40b596947baa42ffbf60e79d5511
Zilfs <rheinlandcast@gmail.com>
2023-10-02 11:08:15 +0700

    tambah model siswa dan relasi

---
+++ app/Models/Siswa.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Siswa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'siswa';
+
+    protected $fillable = [
+        'NIS', 
+        'nama',
+        'id_kelas'
+    ];
+
+    protected $hidden = [
+
+    ];
+
+    public function kelas()
+    {
+        return $this->belongsTo(Kelas::class, 'id_kelas', 'id');
+    }
+}
