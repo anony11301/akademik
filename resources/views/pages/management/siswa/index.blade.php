5489cc53c2a89b091767aee0712fdf52a6dd2bc5
Zilfs <rheinlandcast@gmail.com>
2023-10-02 13:44:57 +0700

    update view siswa

--- resources/views/pages/management/siswa/index.blade.php
+++ resources/views/pages/management/siswa/index.blade.php
@@ -3,7 +3,7 @@
     <div class="row px-5 py-5">
 
         @foreach ($kelas as $item)
-            <div class="col col-12 col-sm-8 col-xl-4 col-md-6 mb-4">
+            <div class="col col-12 col-xl-4 col-md-6 mb-4">
                 <div class="card border-left-warning shadow h-100 py-2">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
