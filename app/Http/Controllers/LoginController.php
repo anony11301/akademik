378dae698df65a6fba2baff157d686b0e062c00c
Zilfs <rheinlandcast@gmail.com>
2023-10-02 11:28:51 +0700

    update controller login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,7 +10,7 @@ class LoginController extends Controller
     //
     public function index() 
     {
-        return view('auth.login');
+        return view('pages.auth.login');
     }
 
     public function login_proses(Request $request) {
