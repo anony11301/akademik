8528fd73322a5e224b312159d109384e336a2653
DikaPutra1205 <duia.putra12@gmail.com>
2023-10-11 10:25:25 +0700

    auk

--- resources/views/pages/guru/absen/detail.blade.php
+++ resources/views/pages/guru/absen/detail.blade.php
@@ -5,35 +5,31 @@
         <h1 class="mb-4">Data Kelas</h1>
         <form action="" method="get">
             <div class="row mb-3">
-                <div class="col-md-5 form-group">
+                <div class="col-md-3 form-group">
                     <label for="date_from">Dari Tanggal</label>
                     <input type="date" name="date_from" class="form-control" value="{{ $request->date_from }}">
                 </div>
-                <div class="col-md-5 form-group">
-                    <label for="date_to">Sampai Tanggal .</label>
+                <div class="col-md-3 form-group">
+                    <label for="date_to">Sampai Tanggal</label>
                     <input type="date" name="date_to" class="form-control" value="{{ $request->date_to }}">
                 </div>
-                <div class="col-md-2 form-group mt-4">
-                    <button type="submit" class="btn btn-primary">Cari</button>
-                </div>
-                <div class="col-md-2 form-group mt-4">
+                <div class="col-md-3 form-group">
                     <label for="status_filter">Filter Status</label>
                     <select class="form-control" name="status_filter" id="status_filter">
                         <option value="">Semua</option>
-                        <option value="hadir" {{ $request->input('status_filter') == 'hadir' ? 'selected' : '' }}>Hadir
-                        </option>
-                        <option value="sakit" {{ $request->input('status_filter') == 'sakit' ? 'selected' : '' }}>Sakit
-                        </option>
-                        <option value="izin" {{ $request->input('status_filter') == 'izin' ? 'selected' : '' }}>Izin
-                        </option>
-                        <option value="alpha" {{ $request->input('status_filter') == 'alpha' ? 'selected' : '' }}>Alpha
-                        </option>
+                        <option value="hadir" {{ $request->input('status_filter') == 'hadir' ? 'selected' : '' }}>Hadir</option>
+                        <option value="sakit" {{ $request->input('status_filter') == 'sakit' ? 'selected' : '' }}>Sakit</option>
+                        <option value="izin" {{ $request->input('status_filter') == 'izin' ? 'selected' : '' }}>Izin</option>
+                        <option value="alpha" {{ $request->input('status_filter') == 'alpha' ? 'selected' : '' }}>Alpha</option>
                     </select>
+                </div>
+                <div class="col-md-1 mt-4 p-2">
+                    <button type="submit" class="btn btn-primary w-100">Cari</button>
                 </div>
             </div>
         </form>
-        <div class="">
-            Presentase Kehadiran {{ number_format($persentasi_kehadiran) }} %
+        <div class="mb-3">
+            <h5>Presentase Kehadiran: {{ number_format($persentasi_kehadiran) }}%</h5>
         </div>
         <div class="table-responsive">
             <table class="table table-hover">
@@ -61,7 +57,6 @@
                             <td>{{ $item->tanggal }}</td>
                         </tr>
                     @endforeach
-
                 </tbody>
             </table>
         </div>
