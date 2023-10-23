d4ec9a7c1d15f8b912b54fefe82dba8525b32e2f
Zilfs <rheinlandcast@gmail.com>
2023-10-23 13:05:19 +0700

    update view siswa

--- resources/views/pages/management/siswa/detail.blade.php
+++ resources/views/pages/management/siswa/detail.blade.php
@@ -10,147 +10,155 @@
                 <div class="container">
                     {{-- notifikasi form validasi --}}
                     @if ($errors->has('file'))
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $errors->first('file') }}</strong>
-                    </span>
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('file') }}</strong>
+                        </span>
                     @endif
-            
+
                     {{-- notifikasi sukses --}}
                     @if ($sukses = Session::get('sukses'))
-                    <div class="alert alert-success alert-block">
-                        <button type="button" class="close" data-dismiss="alert">×</button> 
-                        <strong>{{ $sukses }}</strong>
-                    </div>
+                        <div class="alert alert-success alert-block">
+                            <button type="button" class="close" data-dismiss="alert">×</button>
+                            <strong>{{ $sukses }}</strong>
+                        </div>
                     @endif
 
-                @if (Auth::user()->id_level == 1)
-                    <a href="{{ route('management-tambah-siswa', $id_kelas) }}"
-                        class="btn btn-sm btn-primary float-right mt-2">+
-                        Tambah
-                        Data</a>
-                    <button type="button" class="btn btn-primary mr-5" data-toggle="modal" data-target="#importExcel">IMPORT EXCEL</button>
-                    <!-- Import Excel -->
-                    <div class="modal fade" id="importExcel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <form method="post" action="{{route ('import-siswa', $id_kelas)}}" enctype="multipart/form-data">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">Import Excel</h5>
-                                    </div>
-                                    <div class="modal-body">
-            
-                                        {{ csrf_field() }}
-            
-                                        <label>Pilih file excel</label>
-                                        <div class="form-group">
-                                            <input type="file" name="file" required="required">
-                                        </div>
-            
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                        <button type="submit" class="btn btn-primary">Import</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-
-                    <a href="{{ route('excel-export-siswa', $id_kelas) }}" class="btn btn-sm btn-success float-right mr-2 mt-2">Export Data</a>
-                @else
-                    <a href="{{ route('excel-export-siswa', $id_kelas) }}" class="btn btn-sm btn-success float-right mr-2 mt-2">Export Data</a>
-                @endif
-
-
-            </div>
-            <div class="table-responsive">
-                <table class="table table-hover">
-                    <thead style="color: black">
-                        <tr>
-                            @if (Auth::user()->id_level == 1)
-                                <th scope="col">#</th>
-                                <th scope="col" style="white-space: nowrap">NIS</th>
-                                <th scope="col" style="white-space: nowrap">Nama Siswa</th>
-                                <th scope="col" style="white-space: nowrap">Poin</th>
-                                <th scope="col">Aksi</th>
-                            @else
-                                <th scope="col">#</th>
-                                <th scope="col" style="white-space: nowrap">NIS</th>
-                                <th scope="col" style="white-space: nowrap">Nama Siswa</th>
-                                <th scope="col" style="white-space: nowrap">Poin</th>
-                            @endif
-
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @php
-                            $no = 1;
-                        @endphp
-                        @foreach ($siswa as $item)
-                            <tr>
-                                @if (Auth::user()->id_level == 1)
-                                    <td>{{ $no++ }}</td>
-                                    <td style="white-space: nowrap">{{ $item->NIS }}</td>
-                                    <td style="white-space: nowrap">{{ $item->nama }}</td>
-                                    <td style="white-space: nowrap">{{ $item->poin }}</td>
-                                    <td class="w-25">
-                                        <div class="d-flex">
-                                            <div class="w-50 mx-2 ">
-                                                <a href="{{ route('edit-siswa', $item->NIS) }}"
-                                                    class="btn btn-sm btn-warning  w-100"><i
-                                                        class="fa-solid fa-pen-to-square"></i></a>
-                                            </div>
-                                            <div class="w-50 mx-2">
-                                                <button type="button" class="btn btn-sm btn-danger w-100"
-                                                    data-toggle="modal" data-target="#hapusModal"
-                                                    data-id="{{ $item->NIS }}">
-                                                    <i class="fa-solid fa-trash-can"></i>
-                                                </button>
-                                            </div>
-
-                                        </div>
-                                    </td>
-                                @else
-                                    <td>{{ $no++ }}</td>
-                                    <td style="white-space: nowrap">{{ $item->NIS }}</td>
-                                    <td style="white-space: nowrap">{{ $item->nama }}</td>
-                                    <td style="white-space: nowrap">{{ $item->poin }}</td>
-
-                                @endif
-
-                            </tr>
-                            <!-- Modal -->
-                            <div class="modal fade" id="hapusModal" tabindex="-1" role="dialog"
-                                aria-labelledby="hapusModalLabel" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
+                    @if (Auth::user()->id_level == 1)
+                        <a href="{{ route('management-tambah-siswa', $id_kelas) }}"
+                            class="btn btn-sm btn-primary float-right mb-2">+
+                            Tambah
+                            Data</a>
+                        <a href="#" class="btn btn-primary btn-sm mr-2 float-right mb-2" data-toggle="modal"
+                            data-target="#importExcel">Import
+                            Data</a>
+                        <!-- Import Excel -->
+                        <div class="modal fade" id="importExcel" tabindex="-1" role="dialog"
+                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <form method="post" action="{{ route('import-siswa', $id_kelas) }}"
+                                    enctype="multipart/form-data">
+                                    @csrf
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <h5 class="modal-title" id="hapusModalLabel">Konfirmasi Hapus Siswa</h5>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
+                                            <h5 class="modal-title" id="exampleModalLabel">Import Excel</h5>
                                         </div>
                                         <div class="modal-body">
-                                            Apakah Anda yakin ingin menghapus siswa ini?
+
+                                            {{ csrf_field() }}
+
+                                            <label>Pilih file excel</label>
+                                            <div class="form-group">
+                                                <input type="file" name="file" required="required">
+                                            </div>
+
                                         </div>
                                         <div class="modal-footer">
                                             <button type="button" class="btn btn-secondary"
-                                                data-dismiss="modal">Batal</button>
-                                            <form id="delete-form" method="POST"
-                                                action="{{ route('delete-siswa', $item->NIS) }}">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger">Hapus</button>
-                                            </form>
+                                                data-dismiss="modal">Close</button>
+                                            <button type="submit" class="btn btn-primary">Import</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+
+                        </div>
+                        <a href="{{ route('excel-export-siswa', $id_kelas) }}"
+                            class="btn btn-sm btn-success float-right mr-2 mb-2">Export Data</a>
+                    @else
+                        <a href="{{ route('excel-export-siswa', $id_kelas) }}"
+                            class="btn btn-sm btn-success float-right mr-2 mt-2">Export Data</a>
+                    @endif
+
+
+                </div>
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead style="color: black">
+                            <tr>
+                                @if (Auth::user()->id_level == 1)
+                                    <th scope="col">#</th>
+                                    <th scope="col" style="white-space: nowrap">NIS</th>
+                                    <th scope="col" style="white-space: nowrap">Nama Siswa</th>
+                                    <th scope="col" style="white-space: nowrap">Poin</th>
+                                    <th scope="col">Aksi</th>
+                                @else
+                                    <th scope="col">#</th>
+                                    <th scope="col" style="white-space: nowrap">NIS</th>
+                                    <th scope="col" style="white-space: nowrap">Nama Siswa</th>
+                                    <th scope="col" style="white-space: nowrap">Poin</th>
+                                @endif
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php
+                                $no = 1;
+                            @endphp
+                            @foreach ($siswa as $item)
+                                <tr>
+                                    @if (Auth::user()->id_level == 1)
+                                        <td>{{ $no++ }}</td>
+                                        <td style="white-space: nowrap">{{ $item->NIS }}</td>
+                                        <td style="white-space: nowrap">{{ $item->nama }}</td>
+                                        <td style="white-space: nowrap">{{ $item->poin }}</td>
+                                        <td class="w-25">
+                                            <div class="d-flex">
+                                                <div class="w-50 mx-2 ">
+                                                    <a href="{{ route('edit-siswa', $item->NIS) }}"
+                                                        class="btn btn-sm btn-warning  w-100"><i
+                                                            class="fa-solid fa-pen-to-square"></i></a>
+                                                </div>
+                                                <div class="w-50 mx-2">
+                                                    <button type="button" class="btn btn-sm btn-danger w-100"
+                                                        data-toggle="modal" data-target="#hapusModal"
+                                                        data-id="{{ $item->NIS }}">
+                                                        <i class="fa-solid fa-trash-can"></i>
+                                                    </button>
+                                                </div>
+
+                                            </div>
+                                        </td>
+                                    @else
+                                        <td>{{ $no++ }}</td>
+                                        <td style="white-space: nowrap">{{ $item->NIS }}</td>
+                                        <td style="white-space: nowrap">{{ $item->nama }}</td>
+                                        <td style="white-space: nowrap">{{ $item->poin }}</td>
+                                    @endif
+
+                                </tr>
+                                <!-- Modal -->
+                                <div class="modal fade" id="hapusModal" tabindex="-1" role="dialog"
+                                    aria-labelledby="hapusModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="hapusModalLabel">Konfirmasi Hapus Siswa</h5>
+                                                <button type="button" class="close" data-dismiss="modal"
+                                                    aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Apakah Anda yakin ingin menghapus siswa ini?
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-dismiss="modal">Batal</button>
+                                                <form id="delete-form" method="POST"
+                                                    action="{{ route('delete-siswa', $item->NIS) }}">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger">Hapus</button>
+                                                </form>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        @endforeach
-                    </tbody>
-                </table>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+
+
             </div>
-
-
-        </div>
-    @endsection
+        @endsection
