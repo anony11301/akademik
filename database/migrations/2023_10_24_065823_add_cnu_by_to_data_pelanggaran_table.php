cba149e41e6764f8cdff69d9a60812bc689d4477
DikaPutra1205 <duia.putra12@gmail.com>
2023-10-24 13:58:44 +0700

    cnu

---
+++ database/migrations/2023_10_24_065823_add_cnu_by_to_data_pelanggaran_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('data_pelanggaran', function (Blueprint $table) {
+            $table->integer('created_by');
+            $table->integer('updated_by');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
