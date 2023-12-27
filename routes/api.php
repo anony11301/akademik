90cc7932a121bd3e0d6e3d5168c7bc5d0110c9d1
Zilfs <rheinlandcast@gmail.com>
2023-12-27 14:57:56 +0700

    api siswa

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api;
+use App\Http\Controllers\Api\SiswaApi;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/siswa', [SiswaApi::class, 'index']);
