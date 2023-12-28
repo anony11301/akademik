61c4aca9d6410ce0e973921f987ebf775ba5b971
Zilfs <rheinlandcast@gmail.com>
2023-12-28 23:28:23 +0700

    feat(kelas api): show all

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api;
+use App\Http\Controllers\Api\KelasApi;
 use App\Http\Controllers\Api\SiswaApi;
 
 /*
@@ -21,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/siswa', [SiswaApi::class, 'index']);
+Route::get('/kelas', [KelasApi::class, 'index']);
