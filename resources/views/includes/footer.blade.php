4d1e89ca5910c63a891fcbb174d8a16a245ee31b
DikaPutra1205 <duia.putra12@gmail.com>
2024-01-17 07:43:10 +0700

    footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,7 +2,7 @@
  <footer class="sticky-footer bg-white">
      <div class="container my-auto">
          <div class="copyright text-center my-auto">
-             <span>Copyright &copy; Created by Hirzi, Dika, Fredik. Siakad SMK 2023</span>
+             <span>Copyright &copy; Orens Studio 2023</span>
          </div>
      </div>
  </footer>
